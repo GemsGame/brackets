9f3114b763cbeb02b3e68473f1fef459cdc90641
GemsGame <a.masanovets@gmail.com>
2019-03-15 00:40:51 +0300

    feat: add stack algorithm v.2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
   const stack = [];
   for(i=0;i<str.length;i++) {
-   
-      if(str[i] == '(' || str[i] == '{' || str[i] == '[' ){
-          stack.push(str[i]);
-      }
-  
-      if((str[i] == ')') && str[i-1] == '(')
-      {
-          stack.pop();   
-      }
-      
-      if((str[i] == '}') && str[i-1] == '{')
-      {
-          stack.pop();   
-      }
+  //console.log(str);
+      for(b=0;b<bracketsConfig.length;b++) {
 
-      
-      if((str[i] == ']') && str[i-1] == '[')
-      {
-          stack.pop();   
-      }
+        if(str[i] == openBrackets(bracketsConfig)[b]){
+            stack.push(str[i]); //Если элемент является открывающей скобкой - добавляем в стек
+          }
+          
+          if((str[i] == closeBrackets(bracketsConfig)[b]) && stack[stack.length - 1] == openBrackets(bracketsConfig)[b]) 
+          {
+              stack.pop();         
+          }
+         /* if(str[i] == closeBrackets(bracketsConfig)[b]){ // прилетел элемент  }
+           if ( (str[i] == ")") && stack[stack.length - 1] == "("|| (str[i] == "}") && stack[stack.length - 1] == "{"||
+           (str[i] == "]") && stack[stack.length - 1] == "["|| (str[i] == "|") && stack[stack.length - 1] == "|") {
+             //дикая проверка
+            stack.pop(); 
+            } */
+           /*
+            Если элемент является закрывающей скобкой - посмотрите, соответствует ли он последнему элементу в стеке. 
+            Если это так, удалите из стека последний элемент и перейдите к следующему элементу. */ 
+    }
   }
-  if(stack.length == 0){
+  console.log(stack);
+  if(stack.length == 0){      
       return true;
-  } return false;
+  } else  return false;
+}
+
+const openBrackets = (bracketsConfig) => {
+    return bracketsConfig.map(item => item[0]);
+}
+
+const closeBrackets = (bracketsConfig) => {
+    return bracketsConfig.map(item => item[1]);
 }
